6254755f8188187108f6a9d2a4840cc2b69fed01
BASAVARAJ HADIMANI <106008685+basavarajh99@users.noreply.github.com>
2022-08-06 22:17:23 +0530

    Update Reverse-array.cpp

--- Reverse-array.cpp
+++ Reverse-array.cpp
@@ -5,5 +5,16 @@ void reverse(l, r, a)
   
   swap(a[l], a[r]);
   
-  reverse(l+1, r-1);
+  reverse(l+1, r-1, a);
+}
+
+// or
+
+void rev(i,a)
+{
+  if(i>=n/2) return;
+  
+  swap(a[i], a[n-i-1]);
+  
+  rev(i+1, a);
 }
