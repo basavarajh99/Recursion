6522c2abf7494a72dea7753bc57cb88293778c1b
BASAVARAJ HADIMANI <106008685+basavarajh99@users.noreply.github.com>
2022-08-07 18:13:31 +0530

    Update All-Subsequence-k-sum.cpp

--- All-Subsequence-k-sum.cpp
+++ All-Subsequence-k-sum.cpp
@@ -22,3 +22,31 @@ void print(int i, vector<int> v, int s, int sum, int arr[], int n)
   s-=arr[i];
   print(i+1, v, s, sum, arr, n);
 }
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//To print only one subsequence
+bool print(int i, vector<int> v, int s, int sum, int arr[], int n)
+{
+  if(i==n)
+  {
+    if(s==sum)
+    {
+      for(auto it : v)
+        cout<<it;
+      
+      return true;
+    }
+    else return false;
+  }
+  
+  //pick
+  v.push_back(arr[i]);
+  s+=arr[i];
+  if(print(i+1, v, s, sum, arr, n)) return true; //no more recursion calls
+  
+  //not pick
+  v.pop_back(arr[i]);
+  s-=arr[i];
+  if(print(i+1, v, s, sum, arr, n)) return true; //no more recursion calls
+  
+  return false;
+}
