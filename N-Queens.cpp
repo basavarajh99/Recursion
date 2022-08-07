ea9586ab4b5f4bad83269f79214a474768c1eba3
BASAVARAJ HADIMANI <106008685+basavarajh99@users.noreply.github.com>
2022-08-07 22:27:32 +0530

    Update N-Queens.cpp

--- N-Queens.cpp
+++ N-Queens.cpp
@@ -1,3 +1,5 @@
+//Brute-force
+
 //only check for left half while issafe()
 class Solution {
 public:
@@ -61,3 +63,51 @@ public:
         return ans;
     }
 };
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    //efficient, by hashing that is check whether a queen is placed in that row before this avoids O(N) while checking for directions in isSafe()
+    //mark hash[row+col] = 1 so that after u can check whether a queen is placed in that diagonal or not, this is for south-west
+    //for north-west, hash[(n-1)+(col-row)] = 1
+    //for west hash[row] = 1
+    
+    class Solution {
+public:    
+    void solve(int col, vector<string>& board, vector<vector<string>>& ans, int n, vector<int>& west, vector<int>& north_west, vector<int>& south_west)
+    {
+        if(col == n) //once col goes out of range
+        {
+            ans.push_back(board);
+            return;
+        }
+        
+        for(int row=0; row<n; row++)// try for every row
+        {
+            if(west[row]==0 and south_west[row+col]==0 and north_west[n-1 + col-row]==0) //is it safe to place queen in that row so that it doesn't get attacked 
+            {
+                board[row][col]='Q';
+                //mark in all three directions
+                west[row] = 1; 
+                south_west[row+col] = 1;
+                north_west[n-1 + col-row] = 1;
+                solve(col+1, board, ans, n, west, north_west, south_west);
+                //remove once recursion over i.e, backtrack
+                board[row][col]='.'; 
+                west[row] = 0; 
+                south_west[row+col] = 0;
+                north_west[n-1 + col-row] = 0;
+            }
+        }
+    }
+    
+    vector<vector<string>> solveNQueens(int n) {
+       vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n, '.');
+        for(int i=0; i<n; i++) //placing empty string in every row of board(nxn)
+            board[i] = s;
+        vector<int> west(n, 0), north_west(2*n-1, 0), south_west(2*n-1, 0);
+        solve(0, board, ans, n, west, north_west, south_west);
+        return ans;
+    }
+};
+    
+    
